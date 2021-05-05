6f43325c31abcfed57d96bbee9b1f56e6961c3b5
NoneType <74427907+Nonetypee@users.noreply.github.com>
2021-05-05 20:38:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,31 +1,29 @@
 
-###Unutmayınız Botta Guild ID tanımı bulunmadığından sadece 1 sunucuda deneyiniz ve kanalları isimleriyle settings.py dosyasına ekleyiniz.###
+# ID olarak doldurunuz ve lütfen js mantığı ile "" koymayınız. (Örnek : yetki1 = 5123145614894185)
 
-
-#Token
-token = 'ODM4MDUyNjQzMDYyMjg0NDAw.YI1fWA.7iUBRXJ2HjbkohBk_RhFLPnet-8'
+#Bot ready
+token = 'TOKEN'
 ReadyStatus = "Devotion"
-prefix = "+"
+prefix = "."
+
 
 #Sunucu özel
-tag1 = ""
-yetki1=836330582392438854
-yetki2=836330575820095518
-yetki3=837784429702807632
-yetki4=836330583420305458
+tag1 = 
+yetki1=
+yetki2=
+yetki3=
+yetki4=
 
 
 #Yazı kanalları 
-cezalog = 837455830215163925
-welcome = 837784494043299840
-welcomelog=837537614839939073
-rolelog=837455830215163925
-
-#Role (Role ismi olarak girilecektir ID girmeyiniz. Sunucuya giren üyelere verilecek AutoRole'ü giriniz.)
-
-unregister=836330582392438854
+cezalog = 
+welcome = 
+welcomelog=
+rolelog=
 
 
+#Role
+autorole=
 
-#Ses kanalları
 
+#NoneType was here
